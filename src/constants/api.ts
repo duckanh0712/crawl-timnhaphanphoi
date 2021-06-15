0addd3e469f9197cc7056eab6d007d5ca0559cd7
duckhanh <duckhanh97pt@gmail.com>
2021-06-15 11:00:12 +0700

    fix jenkinfile

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,3 +2,4 @@ export const TIMNHAPHANPHOI_API = process.env.TIMNHAPHANPHOI_API;
 export const TTS_API = process.env.TTS_API;
 export const CHOZOI_API = process.env.CHOZOI_API;
 export const SENDO_HTTP = process.env.SENDO_HTTP;
+//
